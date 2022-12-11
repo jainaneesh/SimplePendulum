ff8e78151c741af2dcf4632a57ae47e0484470cb
Aneesh Jain <jainaneesh15@gmail.com>
2022-12-11 16:25:10 -0500

    Made a few tweaks to make the animation look prettier

--- Pendulum.py
+++ Pendulum.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 from PIL import Image
-import glob
-import natsort
 import os
 import imageio
 
@@ -22,10 +20,8 @@ path = os.path.join(parent_dir,directory)
 # global count
 
 def pendulum(theta,t,b,g,l,m):
-    # cnt = count
     global count
     global path
-    # global fig
     count = count+1
     theta1 = theta[0]
     theta2 = theta[1]
@@ -34,29 +30,20 @@ def pendulum(theta,t,b,g,l,m):
     dtheta_dt = [dtheta1_dt,dtheta2_dt]    
     x1 = l*math.sin(theta1)
     y1 = -l*math.cos(theta1)
-    filename = 'test%5d.png'%count
-    fig = plt.figure(count)
-    # plt.figure(count)  
-    plt.plot([-2, 2],[0, 0],'b-')
+    num = '{0:04}'.format(count)
+    filename = 'Test' + str(num) + '.png'    
+    fig = plt.figure(count,figsize=(7,7))
+    plt.plot([-2, 2],[0, 0],'b-',linewidth=3)
     plt.plot([x0, x1],[y0, y1],'r-')
     plt.plot(x1,y1,'ro')
-    plt.xlim(-2, 2)
-    plt.ylim(-2, 2)
-    # plt.grid()
-    # plt.axis('equal')
-    # plt.show()        
+    plt.xlim(-1.5, 1.5)
+    plt.ylim(-2, 1)     
     plt.savefig(path + filename)
-    # plt.hold(False)
     plt.close(fig)
-    # count = count + 1
-    # print(count)
     return dtheta_dt
 
 theta_0 = [0,3]
 t = np.linspace(0,20,300)
-# print(len(t))
-# t.reshape(len(t),1)
-# print(t.shape)
 x0 = 0
 y0 = 0
 count = 0
@@ -66,39 +53,21 @@ m = 1
 l = 1
 
 dtheta_dt = odeint(pendulum,theta_0,t,args=(b,g,l,m))
-# print(dtheta_dt.shape)
-# imgs = glob.glob("*.png")
-# images = natsort.natsorted(imgs,reverse = False)
-# frames = []
-# for i in images:
-#     new_frame = Image.open(i)
-#     frames.append(new_frame)
 
-# # # print(type(frames))
-# frames[0].save('Pendulum_Animation_Using_Python.gif',format = 'GIF', append_images = frames[1:], save_all = True, duration = 40, loop = 1)
-
-#Plotting for animation
-# x = np.zeros((len(t),1),dtype=float)
-# y = np.zeros((len(t),1),dtype=float)
-# for i in range(1,len(t)):
-#     x[i] = l*math.sin(dtheta_dt[i,0])
-#     y[i] = -l*math.cos(dtheta_dt[i,0])
-# print(x)
-# print(y)
-# plt.plot(x,y,'r.')
-# plt.grid()
-# plt.show()
+fig = plt.figure(2)
 plt.plot(t,dtheta_dt[:,0],'b-',label=r'$\frac{d\theta_1}{dt}=\theta_2$')
 plt.plot(t,dtheta_dt[:,1],'r--',label=r'$\frac{d\theta_2}{dt}-\frac{b}{m}\theta_2-\frac{g}{l}sin\theta_1$')
 plt.ylabel('Plot')
 plt.xlabel('Time')
 plt.legend(loc='best')
-plt.show()
+plt.savefig(parent_dir + "SimplePendulum.png")
 
 import imageio.v2 as imageio
-with imageio.get_writer("C:/Users/jaina/Desktop/Python/SimplePendulum/pendulum.gif", mode='i') as writer:
+with imageio.get_writer("C:/Users/jaina/Desktop/Python/SimplePendulum/PENDULUM.gif", mode='i') as writer:
     for i in range(1, 1302):
-        filename='test' + str(i)+ '.png'
+        # filename='test' + str(i)+ '.png'
+        num = '{0:04}'.format(i)
+        filename = 'Test' + str(num) + '.png'
         # filename = 'test%5d.png'%i
         # image = imageio.imread("C:/Users/jaina/Desktop/Python/SimplePendulum/Animation/filename")
         image = imageio.imread(path + filename)
